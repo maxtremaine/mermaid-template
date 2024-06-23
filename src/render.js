7a2a0e8349b5631f9fe2b3db27ec710c8ea788bc
Max Tremaine <max.tremaine@gmail.com>
2024-06-23 13:15:32 -0400

    Simplify usage by gathering pre tag ids

--- src/render.js
+++ src/render.js
@@ -1,20 +1,14 @@
 import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";
 
-/**
- * This loop expects the following when imported into an html file:
- * - A list of strings to identify the tables.
- * - Corresponding .mermaid files in a data directory.
- * - Corresponding <pre> tags with id matching table name.
- */
-for (const table of tables) {
-  fetch(`/data/${table}.mermaid`)
+for (const table of document.getElementsByTagName("pre")) {
+  fetch(`/data/${table.id}.mermaid`)
     .then((response) => response.text())
     .then((text) => {
-      document.getElementById(table).innerHTML = text;
+      table.innerHTML = text;
     })
     .then(() => {
       mermaid.run({
-        nodes: [document.getElementById(table)],
+        nodes: [table],
       });
     });
 }
